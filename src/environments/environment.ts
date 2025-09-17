55ae49768329f50497a007e6e8933a4619a32660
gmmonsalve <gbrielamonsalve@gmail.com>
2025-09-16 23:18:42 -0500

    config: add config file

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3001/fly-app/api',
+  path: {
+    flights: {
+      search: '/flights/search'
+    },
+    airports:{
+      getAll: '/airports'
+    }
+  }
+};
