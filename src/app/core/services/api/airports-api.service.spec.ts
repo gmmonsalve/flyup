f087926537d9fde348f3651344961cd0028678f9
gmmonsalve <gbrielamonsalve@gmail.com>
2025-09-23 18:25:23 -0500

    style(api/airports.service): refactor file names to airports-api.service

--- src/app/core/services/api/airports-api.service.spec.ts
+++ src/app/core/services/api/airports-api.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AirportsService } from './airports.service';
+import { AirportsService } from './airports-api.service';
 
 describe('AirportsService', () => {
   let service: AirportsService;
