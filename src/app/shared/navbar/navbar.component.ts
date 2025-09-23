87628607a70f2107de6fb5412969a81057a1ac82
gmmonsalve <gbrielamonsalve@gmail.com>
2025-09-23 13:58:53 -0500

    feat(navbar.component): add navigation to main route by clicking logo

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [MatToolbarModule, MatIconModule, RouterLink, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
