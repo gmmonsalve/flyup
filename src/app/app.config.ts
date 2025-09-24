6daed6693c3e3574ad3a6de3299a77543b2b30db
gmmonsalve <gbrielamonsalve@gmail.com>
2025-09-23 19:06:36 -0500

    feat(booking_config): add more properties to inject for booking rules configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,7 +14,13 @@ export const appConfig: ApplicationConfig = {
       provide: BOOKING_RULES,
       useValue: {
         MAX_PASSENGERS: 9,
-        MIN_PASSENGERS: 1
+        MIN_PASSENGERS: 1,
+        ONE_WAY_FLIGHT_NUMBER: 1,
+        ROUND_TRIP_FLIGHT_NUMBER: 2,
+        TRIP_TYPE: {
+          oneWay: "one-way",
+          roundTrip: "round-trip"
+        }
       }
     }
   ]
--- src/app/core/business/interfaces/bookingRulesConfig.interface.ts
+++ src/app/core/business/interfaces/bookingRulesConfig.interface.ts
@@ -1,4 +1,10 @@
 export default interface BookingRulesConfig {
     MAX_PASSENGERS: number,
-    MIN_PASSENGERS: number
+    MIN_PASSENGERS: number,
+    ONE_WAY_FLIGHT_NUMBER: number,
+    ROUND_TRIP_FLIGHT_NUMBER: number,
+    TRIP_TYPE: {
+        oneWay: string,
+        roundTrip: string
+    }
 } 
