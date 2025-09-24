2d8049127a512c94612f74547dbc6d3b2388d90c
gmmonsalve <gbrielamonsalve@gmail.com>
2025-09-23 19:08:35 -0500

    feat(app.routes): add new routes for flight serach and booking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
+import { bookingGuard } from './presentation/guards/booking.guard';
 
 export const routes: Routes = [
     { path: '', loadComponent: ()=> import('@presentation/pages/search-page/search-page.component') },
+    { path: 'flights/search', loadComponent: ()=> import('@presentation/pages/flights-page/flights-page.component')},
+    { path: 'booking', canMatch: [bookingGuard],
+        children: [
+            { path: 'resume', loadComponent: ()=> import('@app/presentation/pages/booking-resume-page/booking-resume-page.component')},
+        ]
+    },
+    { path: '**', redirectTo: ''}
 ];
