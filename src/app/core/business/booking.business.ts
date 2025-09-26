891062c86949a59b56e696c414f534c8c1095a6a
gmmonsalve <gbrielamonsalve@gmail.com>
2025-09-25 23:18:59 -0500

    feat(booking.bussiness): add method validate number passengers

--- src/app/core/business/booking.business.ts
+++ src/app/core/business/booking.business.ts
@@ -8,10 +8,6 @@ export class BookingRules {
    private RULES = inject<BookingRulesConfig>(BOOKING_RULES); 
 
   validateBookingDetails(booking: Booking): void {
-    if(booking.passengers.length < 1) {
-      throw new Error('At least one passenger is required to proceed with the booking.');
-    }
-
     if(booking.flights.length < 1) {
       throw new Error('At least one flight must be selected to proceed with the booking.');
     }
@@ -19,7 +15,12 @@ export class BookingRules {
     if(booking.selectedSeats.length !== booking.passengers.length) {
       throw new Error('The number of selected seats must match the number of passengers.');
     }
+  }
 
+  validateNumberPassengers(booking: Booking){
+    if(booking.passengers.length < 1) {
+      throw new Error('At least one passenger is required to proceed with the booking.');
+    }
   }
 
   isFlightSelectionComplete(booking: Booking | null): boolean{
